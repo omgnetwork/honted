6b2d95965a19013e55ccc93c4f2bdb9555a0f9e1
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2017-10-06 16:03:34 +0200

    first ugly pass, mocking HonteD.Crypto

---
+++ test/honted/crypto_test.exs
@@ -0,0 +1,18 @@
+defmodule Honted.CryptoTest do
+  use ExUnit.Case
+  @moduledoc """
+  Consider removing if brittle - testing implementation details
+  
+  A sanity check of the crypto-placeholder implementation
+  """
+  test "wrap unwrap sign verify" do
+    {:ok, priv} = HonteD.Crypto.generate_private_key
+    {:ok, pub} = HonteD.Crypto.generate_public_key(priv)
+    {:ok, address} = HonteD.Crypto.generate_address(pub)
+    
+    {:ok, signature} = HonteD.Crypto.sign("message", priv)
+    assert {:ok, true} == HonteD.Crypto.verify("message", signature, address)
+    assert {:ok, false} == HonteD.Crypto.verify("message2", signature, address)
+  end
+  
+end
