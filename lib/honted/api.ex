0db1d9d68588ed24832b7d34129f96b86ce498c7
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2017-10-09 13:17:44 +0200

    quick-comment, keep TendermintRPC explicit - will have many rpcs

--- lib/honted/api.ex
+++ lib/honted/api.ex
@@ -5,7 +5,7 @@ defmodule HonteD.API do
   Should abstract out all Tendermint-related stuff
   """
 
-  alias HonteD.TendermintRPC, as: RPC
+  alias HonteD.TendermintRPC
 
   @spec create_send_transaction(binary, pos_integer, binary, binary) :: {:ok, binary} | any
   def create_send_transaction(asset, amount, from, to)
@@ -14,8 +14,8 @@ defmodule HonteD.API do
        amount > 0 and
        is_binary(from) and
        is_binary(to) do
-    client = RPC.client()
-    case RPC.abci_query(client, "", "/nonces/#{from}") do
+    client = TendermintRPC.client()
+    case TendermintRPC.abci_query(client, "", "/nonces/#{from}") do
       {:ok, %{"response" => %{"code" => 0, "value" => nonce}}} ->
         HonteD.TxCodec.encode([Base.decode16!(nonce), :send, asset, amount, from, to])
       result -> result
@@ -24,8 +24,8 @@ defmodule HonteD.API do
 
   @spec submit_transaction(binary) :: {:ok, binary} | any
   def submit_transaction(transaction) do
-    client = RPC.client()
-    case RPC.broadcast_tx_sync(client, transaction) do
+    client = TendermintRPC.client()
+    case TendermintRPC.broadcast_tx_sync(client, transaction) do
       {:ok, %{"code" => code, "hash" => hash}} when code in [0, 3] ->
         {:ok, hash}  # either submitted or duplicate
       result -> result
@@ -36,8 +36,8 @@ defmodule HonteD.API do
   def query_balance(asset, address)
   when is_binary(asset) and
        is_binary(address) do
-    client = RPC.client()
-    case RPC.abci_query(client, "", "/accounts/#{asset}/#{address}") do
+    client = TendermintRPC.client()
+    case TendermintRPC.abci_query(client, "", "/accounts/#{asset}/#{address}") do
       {:ok, %{"response" => %{"code" => 0, "value" => balance_enc}}} ->
         {balance, ""} = Integer.parse(Base.decode16!(balance_enc))
         {:ok, balance}
