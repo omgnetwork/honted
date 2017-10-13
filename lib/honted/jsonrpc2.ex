c4718d209603760f2df63446496cec309530cc53
Paul Peregud <paulperegud@gmail.com>
2017-10-13 11:41:10 +0200

    make translation process pure
    * this makes it testable

--- lib/honted/jsonrpc2.ex
+++ lib/honted/jsonrpc2.ex
@@ -15,13 +15,22 @@ defmodule HonteD.JSONRPC2.Server.Handler do
   @type function_name :: binary
   @type arg_name :: binary
   @type json_args :: %{required(arg_name) => any}
+  @type rpc_error :: :method_not_found | {:invalid_params, %{}}
+  @type spec :: any
 
   @spec handle_request(endpoint :: function_name, params :: json_args) :: any
   def handle_request(endpoint, params) do
+    with {:ok, fname, args} = translate_request(endpoint, params, @api._spec()),
+         do: apply_call(@api, fname, args)
+  end
+
+  @spec translate_request(endpoint :: function_name, params :: json_args, spec :: spec)
+        :: {:ok, atom, list(any)} | rpc_error
+  def translate_request(endpoint, params, spec) do
     with {:ok, fname} <- existing_atom(endpoint),
-         :ok <- is_exposed(fname),
-         {:ok, args} <- get_args(fname, params),
-         do: apply_call(fname, args)
+         :ok <- is_exposed(fname, spec),
+         {:ok, args} <- get_args(fname, params, spec),
+         do: {:ok, fname, args}
   end
 
   @spec existing_atom(endpoint :: function_name) :: {:ok, atom} | :method_not_found
@@ -33,24 +42,24 @@ defmodule HonteD.JSONRPC2.Server.Handler do
     end
   end
 
-  @spec is_exposed(fname :: atom) :: {:ok, atom} | :method_not_found
-  defp is_exposed(fname) do
-    case fname in Map.keys(@api._spec()) do
+  @spec is_exposed(fname :: atom, spec :: spec) :: {:ok, atom} | :method_not_found
+  defp is_exposed(fname, spec) do
+    case fname in Map.keys(spec) do
       true -> :ok
       false -> :method_not_found
     end
   end
 
-  @spec get_args(fname :: atom, params :: json_args) :: {:ok, list(any)} | {:invalid_params, %{}}
-  defp get_args(fname, params) do
-    argspec = @api._spec()[fname].args
-    validate_args = fn({name, _} = spec, list) ->
+  @spec get_args(fname :: atom, params :: json_args, spec :: spec)
+        :: {:ok, list(any)} | {:invalid_params, %{}}
+  defp get_args(fname, params, spec) do
+    validate_args = fn({name, _} = argspec, list) ->
       case Map.get(params, Atom.to_string(name)) do
-        nil -> {:halt, {:missing_arg, spec}}
+        nil -> {:halt, {:missing_arg, argspec}}
         value -> {:cont, list ++ [value]}
       end
     end
-    case Enum.reduce_while(argspec, [], validate_args) do
+    case Enum.reduce_while(spec[fname].args, [], validate_args) do
       {:missing_arg, {name, type}} ->
         {:invalid_params, %{msg: "Please provide parameter `#{name}` of type `#{inspect type}`"}}
       args ->
@@ -58,8 +67,8 @@ defmodule HonteD.JSONRPC2.Server.Handler do
     end
   end
 
-  defp apply_call(fname, args) do
-    :erlang.apply(@api, fname, args)
+  defp apply_call(module, fname, args) do
+    :erlang.apply(module, fname, args)
   end
 
 end
