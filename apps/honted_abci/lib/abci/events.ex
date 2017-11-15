4ce6d0c7f87d79d60111a0fc1268b64c01b0c1ee
Paul Peregud <paulperegud@gmail.com>
2017-11-15 16:12:50 +0100

    better doc

--- apps/honted_abci/lib/abci/events.ex
+++ apps/honted_abci/lib/abci/events.ex
@@ -1,9 +1,12 @@
 defmodule HonteD.ABCI.Events do
   @moduledoc """
+  Emitting of Events from the ABCI to an outside app handling events
+  """
+
+  @doc """
   Supply metadata for SignOff event - Eventer needs a list of tokens to
   be able to emit finalized events.
   """
-
   def notify(state, %HonteD.Transaction.SignOff{} = tx) do
     case HonteD.ABCI.State.issued_tokens(state, tx.sender) do
       {:ok, tokens} ->
