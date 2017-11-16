8bf2d5b07f22b0e27c2cd0133f69d9325793080e
Paul Peregud <paulperegud@gmail.com>
2017-11-16 16:45:45 +0100

    handling sing-offs - one more test

--- apps/honted_abci/test/abci/events_test.exs
+++ apps/honted_abci/test/abci/events_test.exs
@@ -3,9 +3,9 @@ Code.load_file("../honted_api/test/testlib/api/api_helpers.ex")
 defmodule HonteD.ABCI.EventsTest do
   @moduledoc """
   Tests if Events are processed correctly, by the registered :honted_events app
-  
-  FIXME: think if we shouldn't test using a fixture-provided Eventer GenServer as in HonteD.API-HonteD.Events.Eventer
-         tests. That would allow `async: true`
+
+  FIXME: think if we shouldn't test using a fixture-provided Eventer GenServer as in
+  HonteD.API-HonteD.Events.Eventer tests. That would allow `async: true`
   """
   use ExUnitFixtures
   use ExUnit.Case, async: false
@@ -15,11 +15,15 @@ defmodule HonteD.ABCI.EventsTest do
 
   import HonteD.API.TestHelpers
   import HonteD.ABCI.TestHelpers
-  
+
   @test_eventer HonteD.Events.Eventer
 
   deffixture server do
-    {:ok, _} = GenServer.start(HonteD.Events.Eventer, [], [name: @test_eventer])
+    {:ok, pid} = GenServer.start(HonteD.Events.Eventer, [], [name: @test_eventer])
+    on_exit fn ->
+      Process.exit(pid, :shutdown)
+    end
+    pid
   end
 
   defp receivable({:ok, something}), do: receivable(something)
@@ -36,6 +40,7 @@ defmodule HonteD.ABCI.EventsTest do
                                                    alice: alice, bob: bob,
                                                    issuer: issuer, some_block_hash: hash} do
       # prepare send
+      assert Process.alive?(Process.whereis(@test_eventer))
       {:ok, send_enc} = create_send(nonce: 0, asset: asset, amount: 5, from: alice.addr, to: bob.addr)
       e1 = receivable(sign(send_enc, alice.priv))
       e2 = receivable_finalized(e1)
@@ -49,6 +54,32 @@ defmodule HonteD.ABCI.EventsTest do
       assert status_send_filter?(@test_eventer, pid, bob.addr)
       {:ok, send_enc} |> sign(alice.priv) |> deliver_tx(state) |> success?
       {:ok, signoff_enc} |> sign(issuer.priv) |> deliver_tx(state) |> success?
+      join()
+    end
+
+    @tag fixtures: [:server, :state_bob_has_tokens2, :some_block_hash, :alice, :bob,
+                    :asset, :asset2, :issuer2]
+    test "Sign_off delivers tokens of the issuer who did the sign off",
+      %{state_bob_has_tokens2: state, asset: asset, asset2: asset2,
+        alice: alice, bob: bob,
+        issuer2: issuer2, some_block_hash: hash} do
+      {:ok, send1} = create_send(nonce: 0, asset: asset, amount: 2, from: alice.addr, to: bob.addr)
+      {:ok, send2} = create_send(nonce: 0, asset: asset2, amount: 3, from: bob.addr, to: alice.addr)
+      e1 = receivable(sign(send1, alice.priv))
+      e2 = receivable(sign(send2, bob.priv))
+      [_f1, f2] = receivable_finalized([e1, e2])
+      {:ok, signoff_enc} = create_sign_off(nonce: 2, height: 2, hash: hash, sender: issuer2.addr)
+      pid = client(fn() ->
+        assert_receive(^e1)
+        assert_receive(^e2)
+        assert_receive(^f2)
+        refute_receive(_)
+      end)
+      new_send_filter(@test_eventer, pid, alice.addr)
+      new_send_filter(@test_eventer, pid, bob.addr)
+      {:ok, send1} |> sign(alice.priv) |> deliver_tx(state) |> success?
+      {:ok, send2} |> sign(bob.priv) |> deliver_tx(state) |> success?
+      {:ok, signoff_enc} |> sign(issuer2.priv) |> deliver_tx(state) |> success?
       join()
     end
   end
--- apps/honted_abci/test/fixtures.exs
+++ apps/honted_abci/test/fixtures.exs
@@ -17,6 +17,7 @@ defmodule HonteD.ABCI.Fixtures do
       alice: generate_entity(),
       bob: generate_entity(),
       issuer: generate_entity(),
+      issuer2: generate_entity(),
       carol: generate_entity(),
     }
   end
@@ -25,6 +26,7 @@ defmodule HonteD.ABCI.Fixtures do
   deffixture bob(entities), do: entities.bob
   deffixture carol(entities), do: entities.carol
   deffixture issuer(entities), do: entities.issuer
+  deffixture issuer2(entities), do: entities.issuer2
 
   deffixture asset(issuer) do
     # FIXME: as soon as that functionality lands, we should use HonteD.API to discover newly created
@@ -33,9 +35,20 @@ defmodule HonteD.ABCI.Fixtures do
     HonteD.Token.create_address(issuer.addr, 0)
   end
 
+  deffixture asset2(issuer2) do
+    HonteD.Token.create_address(issuer2.addr, 0)
+  end
+
   deffixture state_with_token(empty_state, issuer) do
     %{code: 0, state: state} =
       create_create_token(nonce: 0, issuer: issuer.addr) |> sign(issuer.priv) |> deliver_tx(empty_state)
+    state
+  end
+
+  deffixture state_with_token2(state_with_token, issuer2) do
+    %{code: 0, state: state} =
+      create_create_token(nonce: 1, issuer: issuer2.addr) |> sign(issuer2.priv)
+      |> deliver_tx(state_with_token)
     state
   end
 
@@ -43,6 +56,16 @@ defmodule HonteD.ABCI.Fixtures do
     %{code: 0, state: state} =
       create_issue(nonce: 1, asset: asset, amount: 5, dest: alice.addr, issuer: issuer.addr)
       |> sign(issuer.priv) |> deliver_tx(state_with_token)
+    state
+  end
+
+  deffixture state_bob_has_tokens2(state_alice_has_tokens, bob, issuer2, asset2) do
+    %{code: 0, state: state} =
+      create_create_token(nonce: 0, issuer: issuer2.addr) |> sign(issuer2.priv)
+      |> deliver_tx(state_alice_has_tokens)
+    %{code: 0, state: state} =
+      create_issue(nonce: 1, asset: asset2, amount: 5, dest: bob.addr, issuer: issuer2.addr)
+      |> sign(issuer2.priv) |> deliver_tx(state)
     state
   end
 
