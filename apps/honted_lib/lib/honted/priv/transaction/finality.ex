86a98643a147b4ea16f4a3712efe27c813fbf030
Paul Peregud <paulperegud@gmail.com>
2017-11-21 15:34:34 +0100

    refactor and fix bug in status/4 checking signoff height

--- apps/honted_lib/lib/honted/priv/transaction/finality.ex
+++ apps/honted_lib/lib/honted/priv/transaction/finality.ex
@@ -6,23 +6,26 @@ defmodule HonteD.Transaction.Finality do
   @spec status(tx_height :: HonteD.block_height, HonteD.block_height, binary, binary)
     :: :finalized | :committed | :committed_unknown
   def status(tx_height, signed_off_height, signoff_hash, block_hash) do
-    case {tx_height >= signed_off_height, signoff_hash == block_hash} do
+    case {height_signed_off?(tx_height, signed_off_height),
+          valid_signoff?(signoff_hash, block_hash)} do
       {true, true} -> :finalized
       {false, true} -> :committed
       {_, false} -> :committed_unknown
     end
   end
 
-  @spec valid_signoff?(HonteD.block_hash, HonteD.block_hash) :: boolean
-  def valid_signoff?(hash, hash), do: true
-  def valid_signoff?(_signed_hash, _real_block_hash), do: false
-
   @spec split_finalized_events(Qex.t({HonteD.block_height, any}), HonteD.block_height)
-    :: {[{HonteD.block_height, any}], [{HonteD.block_height, any}]}
+  :: {[{HonteD.block_height, any}], [{HonteD.block_height, any}]}
   def split_finalized_events(queue, signed_off_height) do
     # for a given token will pop the events committed earlier, that are before the signed_off_height
-    is_older = fn({h, _event}) -> h <= signed_off_height end
+    is_older = fn({h, _event}) -> height_signed_off?(h, signed_off_height) end
     Enum.split_while(queue, is_older)
   end
+
+  @spec valid_signoff?(HonteD.block_hash, HonteD.block_hash) :: boolean
+  def valid_signoff?(signed_hash, real_block_hash), do: signed_hash == real_block_hash
+
+  @spec height_signed_off?(HonteD.block_height, HonteD.block_height) :: boolean
+  defp height_signed_off?(height, signed_off), do: height <= signed_off
 
 end
