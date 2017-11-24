72888267fe4070a31bf60810f2e46556b38efc5c
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2017-11-24 10:49:21 +0100

    allowing to test with `mix test --no-start --include integration`
    Test Plan: mix test --no-start --include integration
    
    Reviewers: pawel
    
    Reviewed By: pawel
    
    Differential Revision: https://phabricator.omisego.io/D239

--- apps/honted_integration/test/test_helper.exs
+++ apps/honted_integration/test/test_helper.exs
@@ -1,2 +1,6 @@
+# because we want to use mix test --no-start by default
+[:porcelain, :hackney]
+|> Enum.map(&Application.ensure_all_started/1)
+
 ExUnit.configure(exclude: [integration: true])
 ExUnit.start()
