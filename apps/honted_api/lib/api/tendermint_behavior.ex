c5aeeeac7c1675369e5f7aa16ba62e42dbb8064c
Paul Peregud <paulperegud@gmail.com>
2017-11-28 17:14:45 +0100

    improve specs for behavior and implementation

--- apps/honted_api/lib/api/tendermint_behavior.ex
+++ apps/honted_api/lib/api/tendermint_behavior.ex
@@ -2,12 +2,15 @@ defmodule HonteD.API.TendermintBehavior do
 
   @type result :: {:ok, map} | {:error, any}
   @type tx :: binary
+  @type data :: any
+  @type path :: binary
   @type hash :: binary
   @type client_ref() :: any
 
   @callback client() :: client_ref
   @callback broadcast_tx_sync(client_ref, tx) :: result
   @callback broadcast_tx_commit(client_ref, tx) :: result
+  @callback abci_query(client_ref, data, path) :: result
   @callback tx(client_ref, hash) :: result
   @callback block(client_ref, height :: pos_integer) :: result
 
