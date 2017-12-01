2111ab3ce266b9ecc9b7e6352f045a719b62b6a8
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2017-12-01 14:52:10 +0100

    running a dummy scenario using Streams and Tasks

--- apps/honted_integration/test/integration/performance_test.exs
+++ apps/honted_integration/test/integration/performance_test.exs
@@ -13,40 +13,49 @@ defmodule HonteD.Integration.PerformanceTest do
   @startup_timeout 100
   @await_result_timeout 11000
   
-  deffixture tm_bench(tendermint) do
+  deffixture tm_bench_starter(tendermint) do
     :ok = tendermint
-    # start tendermint and capture the stdout
-    tm_bench = %Porcelain.Process{err: nil, out: tm_bench_out} = Porcelain.spawn_shell(
-      "tm-bench -c 0 localhost:46657",
-      out: :stream,
-    )
-    :ok = wait_for_tm_bench_start(tm_bench_out)
+    fn ->
+      # start tendermint and capture the stdout
+      tm_bench = %Porcelain.Process{err: nil, out: tm_bench_out} = Porcelain.spawn_shell(
+        "tm-bench -c 0 localhost:46657",
+        out: :stream,
+      )
+      :ok = wait_for_tm_bench_start(tm_bench_out)
       
-    on_exit fn -> 
-      Porcelain.Process.stop(tm_bench)
+      {tm_bench, tm_bench_out}
     end
-    
-    {tm_bench, tm_bench_out}
   end
   
   defp wait_for_tm_bench_start(tm_bench_out) do
     HonteD.Integration.Fixtures.wait_for_start(tm_bench_out, "Running ", @startup_timeout)
   end
   
-  @tag fixtures: [:tendermint, :tm_bench]
-  test "send transaction performance", %{tm_bench: {tm_bench, tm_bench_out}} do
-    # FIXME: remove, just smoke testing the fixtures
-    {:ok, issuer_priv} = Crypto.generate_private_key()
-    {:ok, issuer_pub} = Crypto.generate_public_key(issuer_priv)
-    {:ok, issuer} = Crypto.generate_address(issuer_pub)
-    {:ok, raw_tx} = API.create_create_token_transaction(issuer)
-    {:ok, signature} = Crypto.sign(raw_tx, issuer_priv)
-    API.submit_transaction(raw_tx <> " " <> signature)
+  @tag fixtures: [:tendermint, :tm_bench_starter]
+  test "send transaction performance", %{tm_bench_starter: tm_bench_starter} do
+    # FIXME: remove, just smoke testing the 
+    txs = 
+      Stream.interval(0)
+      |> Stream.map(fn _ -> 
+        {:ok, issuer_priv} = Crypto.generate_private_key()
+        {:ok, issuer_pub} = Crypto.generate_public_key(issuer_priv)
+        {:ok, issuer} = Crypto.generate_address(issuer_pub)
+        {:ok, raw_tx} = HonteD.Transaction.create_create_token(nonce: 0, issuer: issuer)
+        {:ok, signature} = Crypto.sign(raw_tx, issuer_priv)
+        raw_tx <> " " <> signature
+      end)
+      
+    {tm_bench, tm_bench_out} = tm_bench_starter.()
+    
+    Task.async(fn ->
+      txs
+      |> Enum.map(fn tx -> API.submit_transaction_async(tx) end)
+    end)
     
     Porcelain.Process.await(tm_bench, @await_result_timeout)
     
     tm_bench_out
     |> Enum.to_list
-    |> IO.inspect
+    |> IO.puts
   end
 end
