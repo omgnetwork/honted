bd82a4a775e2597892c0d82a92efe1c2b7400cf0
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2017-12-29 13:59:39 +0100

    include missed files from umbrella app in credo

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule HonteD.Umbrella.Mixfile do
       start_permanent: Mix.env == :prod,
       deps: deps(),
       preferred_cli_env: [coveralls: :test, "coveralls.html": :test, "coveralls.detail": :test],
-      dialyzer: [ flags: [:error_handling, :race_conditions, :underspecs, :unknown, :unmatched_returns],
-                  plt_add_deps: :transitive,
-                  ignore_warnings: "dialyzer.ignore-warnings"
+      dialyzer: [flags: [:error_handling, :race_conditions, :underspecs, :unknown, :unmatched_returns],
+                 plt_add_deps: :transitive,
+                 ignore_warnings: "dialyzer.ignore-warnings"
                 ],
       test_coverage: [tool: ExCoveralls],
     ]
