9f24d7c37e1df30fc3da1e294abb2548b078b47f
Paul Peregud <paulperegud@gmail.com>
2018-01-16 14:38:28 +0100

    add waiting for block height

--- apps/honted_eth/lib/eth/wait_for.ex
+++ apps/honted_eth/lib/eth/wait_for.ex
@@ -14,6 +14,22 @@ defmodule HonteD.Eth.WaitFor do
     function(fn() -> repeat_until_ok(f) end, 10_000)
   end
 
+  def block_height(n, dev \\ false, timeout \\ 10_000) do
+    f = fn() ->
+      height = HonteD.Eth.Contract.block_height()
+      IO.puts("current height: #{inspect height}; n: #{n}")
+      case height < n do
+        true ->
+          maybe_mine(dev)
+          :repeat
+        false ->
+          {:ok, height}
+      end
+    end
+    rf = fn() -> repeat_until_ok(f) end
+    function(rf, timeout)
+  end
+
   def receipt(txhash, timeout) do
     f = fn() ->
       case eth_get_transaction_receipt(txhash) do
@@ -36,13 +52,18 @@ defmodule HonteD.Eth.WaitFor do
       {:ok, _} = f.()
     catch
       something ->
-        IO.puts("repeat until single clause: #{inspect something}")
         Process.sleep(100)
         repeat_until_ok(f)
       :error, {:badmatch, _} = error ->
-        IO.puts("repeat until double clause: #{inspect {:error, error}}")
         Process.sleep(100)
         repeat_until_ok(f)
     end
+  end
+
+  defp maybe_mine(false), do: :noop
+  defp maybe_mine(true) do
+    {:ok, [addr | _]} = eth_accounts()
+    {:ok, txhash} = eth_send_transaction(%{from: addr, to: addr, value: "0x1"})
+    {:ok, receipt} = receipt(txhash, 1_000)
   end
 end
