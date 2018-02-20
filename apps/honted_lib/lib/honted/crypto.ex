554131b42059c5774920683ea3cefc58e2b07fb0
Paul Peregud <paulperegud@gmail.com>
2018-02-20 15:24:12 +0100

    implement new Crypto API for signatures

--- apps/honted_lib/lib/honted/crypto.ex
+++ apps/honted_lib/lib/honted/crypto.ex
@@ -1,22 +1,80 @@
 defmodule HonteD.Crypto do
   @moduledoc """
-  Mock of a real cryptography API, to be replaced by Ethereum-compliant primitives
+  Signs and validates signatures. Constructed signatures can be used directly
+  in Ethereum with `ecrecover` call.
+
   """
 
-  def hash(message), do: message |> erlang_hash |> Base.encode16
+  @doc """
+  Produces a cryptographic digest of a message.
+
+  TODO: replace hashing function with ethereum's Keccak
+  """
+  def hash(message), do: message |> erlang_hash()
 
   # NOTE temporary function, which will go away when we move to sha3 and eth primitives
   defp erlang_hash(message), do: :crypto.hash(:sha256, message)
 
   @doc """
-  Produce a stand-alone signature.
+  Produce a stand-alone signature for message of arbitrary length. (r,s,v) tuple, 65 bytes long.
   """
-  def signature(unsigned, priv) when is_binary(unsigned) do
-    hash(unsigned <> priv <> "pub")
+  def signature(msg, priv) do
+    msg
+    |> hash()
+    |> signature_digest(priv)
   end
 
-  def verify(unsigned, signature, address), do: {:ok, hash(unsigned <> address) == signature}
-  def generate_private_key, do: {:ok, :rand.uniform |> to_string |> hash |> Kernel.binary_part(0, 37)}
-  def generate_public_key(priv), do: {:ok, priv <> "pub"}
-  def generate_address(pub), do: {:ok, pub}
+  @doc """
+  Produces a stand-alone signature for message hash. (r,s,v) tuple, 65 bytes long.
+  """
+  def signature_digest(digest, priv) when is_binary(digest) and byte_size(digest) == 32 do
+    # note that here signature === <<r::integer-size(256), s::integer-size(256)>>
+    {signature, _r, _s, v} = ExthCrypto.Signature.sign_digest(digest, priv)
+    v = v + 27 # (why 27? see: https://github.com/ethereum/eips/issues/155#issuecomment-253952071)
+    <<signature :: binary-size(64), v :: unsigned-big-integer-unit(8)-size(1)>>
+    # [r,s,v] <- order of encoding
+    # r == signature.r, left-padded with zeroes to 32 bytes
+    # s == signature.s, left-padded with zeroes to 32 bytes
+    # v == recoveryParam; recoveryParam == 28 or 27; one byte
+    # total 65
+  end
+
+  @doc """
+  Verifies if private key corresponding to `address` was used to produce `signature` for
+  this `msg` binary.
+  """
+  @spec verify(binary, binary, binary) :: {:ok, boolean}
+  def verify(msg, signature, address) do
+    {:ok, recovered_address} =
+      msg |> hash()|> recover(signature)
+    {:ok, address == recovered_address}
+  end
+
+  @doc """
+  Recovers address of signer from binary encoded signature - (r,s,v) tuple.
+  """
+  @spec recover(binary, binary) :: binary
+  def recover(digest, packed_signature) when byte_size(digest) == 32 do
+    <<sig :: binary-size(64), v :: unsigned-big-integer-unit(8)-size(1)>> = packed_signature
+    ExthCrypto.Signature.recover(digest, sig, v)
+  end
+
+  @doc """
+  Generates private key. Internally uses OpenSSL RAND_bytes. May throw if there is not enough entropy.
+  TODO: Think about moving to something dependent on /dev/urandom instead. Might be less portable.
+  """
+  def generate_private_key, do: {:ok, :crypto.strong_rand_bytes(32)}
+
+  @doc """
+  Given a private key, returns DER-encoded public key.
+  """
+  def public_key(priv), do: ExthCrypto.Signature.get_public_key(priv)
+
+  @doc """
+  Given public key, returns an address.
+  """
+  def address(pub), do: pub
+
+  def generate_public_key(priv), do: public_key(priv)
+  def generate_address(pub), do: {:ok, address(pub)}
 end
--- apps/honted_lib/test/honted/crypto_test.exs
+++ apps/honted_lib/test/honted/crypto_test.exs
@@ -8,12 +8,39 @@ defmodule HonteD.CryptoTest do
 
   alias HonteD.Crypto
 
+  test "library signature verify" do
+    priv = :crypto.strong_rand_bytes(32)
+    {:ok, pub} = ExthCrypto.Signature.get_public_key(priv)
+    msg = :crypto.strong_rand_bytes(32)
+    {signature, _r, _s, _recovery_id} =
+      ExthCrypto.Signature.sign_digest(msg, priv)
+    assert ExthCrypto.Signature.verify(msg, signature, pub)
+  end
+
+  test "library signature recover" do
+    priv = :crypto.strong_rand_bytes(32)
+    {:ok, pub} = ExthCrypto.Signature.get_public_key(priv)
+    msg = :crypto.strong_rand_bytes(32)
+    {signature, _r, _s, recovery_id} =
+      ExthCrypto.Signature.sign_digest(msg, priv)
+    assert {:ok, ^pub} = ExthCrypto.Signature.recover(msg, signature, recovery_id)
+  end
+
+  test "digest signature" do
+    {:ok, priv} = Crypto.generate_private_key
+    {:ok, pub} = Crypto.generate_public_key(priv)
+    msg = :crypto.strong_rand_bytes(32)
+    sig = Crypto.signature_digest(msg, priv)
+    assert {:ok, ^pub} = Crypto.recover(msg, sig)
+  end
+
   test "wrap unwrap sign verify" do
     {:ok, priv} = Crypto.generate_private_key
     {:ok, pub} = Crypto.generate_public_key(priv)
     {:ok, address} = Crypto.generate_address(pub)
 
     signature = Crypto.signature("message", priv)
+    assert byte_size(signature) == 65
     assert {:ok, true} == Crypto.verify("message", signature, address)
     assert {:ok, false} == Crypto.verify("message2", signature, address)
   end
